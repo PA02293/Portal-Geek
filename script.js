089b83e428b88b4b46b8285f7f3c31e4d961001d
PA02293 <167267758+PA02293@users.noreply.github.com>
2026-01-25 21:46:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const APP_STATE = {
     searchTimeout: null, // Timer para o Debounce
 
     // 🔗 SEU BACKEND (Verifique se o Tunnelmole está ativo neste link)
-    API_URL: 'https://sfddt8-ip-31-57-60-2.tunnelmole.net' 
+    API_URL: 'https://qbxgwh-ip-31-57-60-2.tunnelmole.net' 
 };
 
 /* ───────────────────────────────────────────────────────────── */
